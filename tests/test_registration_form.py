e806630f11de682906fc0a6b570328de0be944e0
evgenii klimashko <klimashko108@gmail.com>
2023-03-30 19:17:49 +0300

    Частично упаковвал код в registration_page

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -6,16 +6,56 @@ import tests
 from tests import resources
 
 
+class RegistrationPage:
+    def open(self):
+        browser.open('/automation-practice-form')
+        browser.all('[id^=google_ads][id$=container__]').with_(
+            timeout=10).wait_until(
+            have.size_greater_than_or_equal(3)
+        )
+        browser.all('[id^=google_ads][id$=container__]').perform(
+            command.js.remove)
+
+    def type_first_name(self, value):
+        browser.element('#firstName').type(value)
+
+    def fill_date_of_birth(self, year, month, day):
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').type(month)
+        browser.element('.react-datepicker__year-select').type(year)
+        browser.element(f'.react-datepicker__day--0{day}').click()
+
+    def should_registered_user_with(self, full_name, email, *tbd ):
+        browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                full_name,
+                email,
+                'Male',
+                '9079079079',
+                '15 June,1986',
+                'Computer Science',
+                'Reading',
+                'foto.jpg',
+                '1478 Custer Street',
+                'NCR Delhi',
+            )
+        )
+
+
 def test_reg_form():
-    browser.open('/automation-practice-form')
-    browser.all('[id^=google_ads][id$=container__]').with_(
-        timeout=10).wait_until(
-        have.size_greater_than_or_equal(3)
-    )
-    browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
+    registration_page = RegistrationPage()
+    registration_page.open()
+
+    # browser.open('/automation-practice-form')
+    # browser.all('[id^=google_ads][id$=container__]').with_(
+    #     timeout=10).wait_until(
+    #     have.size_greater_than_or_equal(3)
+    # )
+    # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
 
     # WHEN
-    browser.element('#firstName').type('John')
+    registration_page.type_first_name('John')
+    # browser.element('#firstName').type('John')
     browser.element('#lastName').type('Smith')
 
     browser.element('#userEmail').type('name@example.com')
@@ -25,10 +65,11 @@ def test_reg_form():
 
     browser.element('#userNumber').type('9079079079')
 
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').type('June')
-    browser.element('.react-datepicker__year-select').type('1986')
-    browser.element(f'.react-datepicker__day--0{15}').click()
+    registration_page.fill_date_of_birth('1986', 'June', 15)
+    # browser.element('#dateOfBirthInput').click()
+    # browser.element('.react-datepicker__month-select').type('June')
+    # browser.element('.react-datepicker__year-select').type('1986')
+    # browser.element(f'.react-datepicker__day--0{15}').click()
 
     browser.element('#subjectsInput').type('Computer Science').press_enter()
 
@@ -69,17 +110,16 @@ def test_reg_form():
         have.text('Thanks for submitting the form')
     )
 
-    browser.element('.table').all('td').even.should(
-        have.exact_texts(
-            'John Smith',
-            'name@example.com',
-            'Male',
-            '9079079079',
-            '15 June,1986',
-            'Computer Science',
-            'Reading',
-            'foto.jpg',
-            '1478 Custer Street',
-            'NCR Delhi',
+    registration_page.should_registered_user_with(
+        'John Smith',
+        'name@example.com',
+        'Male',
+        '9079079079',
+        '15 June,1986',
+        'Computer Science',
+        'Reading',
+        'foto.jpg',
+        '1478 Custer Street',
+        'NCR Delhi',
         )
-    )
+
