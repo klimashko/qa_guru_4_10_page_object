3a019742de30fdca1e2c1f85158a66ade18c244e
evgenii klimashko <klimashko108@gmail.com>
2023-03-31 21:38:58 +0300

    Упаковал весь код в registration_page, те реализовал подход PageObject

---
+++ qa_guru_4_10_page_object/pages/registration_page.py
@@ -0,0 +1,105 @@
+import os
+
+from selene import have, command, be
+from selene.support.shared import browser
+
+import tests
+
+
+class RegistrationPage:
+
+
+    def open(self):
+        browser.open('/automation-practice-form')
+        browser.all('[id^=google_ads][id$=container__]').with_(
+            timeout=10).wait_until(
+            have.size_greater_than_or_equal(3)
+        )
+        browser.all('[id^=google_ads][id$=container__]').perform(
+            command.js.remove)
+
+    def fill_first_name(self, value):
+        browser.element('#firstName').type(value)
+
+    def fill_last_name(self, value):
+        browser.element('#lastName').type(value)
+        
+    def fill_email(self, value):
+        browser.element('#userEmail').type(value)
+
+
+    def fill_gender(self, value):
+        browser.all('[name=gender]').element_by(have.value(value)).element(
+        '..').click()
+
+    def fill_mobile(self, value):
+        browser.element('#userNumber').type(value)
+
+    def fill_date_of_birth(self, year, month, day):
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').type(month)
+        browser.element('.react-datepicker__year-select').type(year)
+        browser.element(f'.react-datepicker__day--0{day}').click()
+
+    def fill_subject(self, value):
+        browser.element('#subjectsInput').type(value).press_enter()
+
+    def fill_hobby(self, value):
+        browser.all('[for^=hobbies]').element_by(have.exact_text(value)).perform(
+            command.js.scroll_into_view)
+        browser.all('[for^=hobbies]').element_by(have.exact_text(value)).click()
+
+    def fill_picture(self, value):
+        browser.element('#uploadPicture').set_value(
+            os.path.abspath(
+                os.path.join(os.path.dirname(tests.__file__), value)
+            )
+        )
+
+    def fill_address(self, value):
+        browser.element('#currentAddress').type(value)
+
+
+
+    def fill_state(self, value):
+        browser.element('#state').click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(value)
+        ).click()
+
+    def fill_city(self, value):
+        browser.element('#city').click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(value)
+        ).click()
+
+    @property
+    def submit_data(self):
+        browser.element('#submit').perform(command.js.click)
+
+    @property
+    def assert_table_display(self):
+        browser.element('[id=example-modal-sizes-title-lg]').should(be.visible)
+
+    @property
+    def registered_user_data(self):
+        return browser.element('.table').all('td').even
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,35 +1,48 @@
-from selene.support.shared import browser
-from selene import have, by, be
-from selene import command
-import os
-import tests
-from tests import resources
+from selene import have
+from qa_guru_4_10_page_object.pages.registration_page import RegistrationPage
 
 
-class RegistrationPage:
-    def open(self):
-        browser.open('/automation-practice-form')
-        browser.all('[id^=google_ads][id$=container__]').with_(
-            timeout=10).wait_until(
-            have.size_greater_than_or_equal(3)
-        )
-        browser.all('[id^=google_ads][id$=container__]').perform(
-            command.js.remove)
+def test_reg_form():
+    registration_page = RegistrationPage()
 
-    def type_first_name(self, value):
-        browser.element('#firstName').type(value)
+    registration_page.open()
 
-    def fill_date_of_birth(self, year, month, day):
-        browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
-        browser.element(f'.react-datepicker__day--0{day}').click()
+    # WHEN
 
-    def should_registered_user_with(self, full_name, email, *tbd ):
-        browser.element('.table').all('td').even.should(
+    registration_page.fill_first_name('John')
+
+    registration_page.fill_last_name('Smith')
+
+    registration_page.fill_email('name@example.com')
+
+    registration_page.fill_gender('Male')
+
+    registration_page.fill_mobile('9079079079')
+
+    registration_page.fill_date_of_birth('1986', 'June', 15)
+
+    registration_page.fill_subject('Computer Science')
+
+    registration_page.fill_hobby('Reading')
+
+    registration_page.fill_picture('resources/foto.jpg')
+
+    registration_page.fill_address('1478 Custer Street')
+
+    registration_page.fill_state('NCR')
+
+    registration_page.fill_city('Delhi')
+
+    registration_page.submit_data
+
+    # THEN
+
+    registration_page.assert_table_display
+
+    registration_page.registered_user_data.should(
             have.exact_texts(
-                full_name,
-                email,
+                'John Smith',
+                'name@example.com',
                 'Male',
                 '9079079079',
                 '15 June,1986',
@@ -39,87 +52,5 @@ class RegistrationPage:
                 '1478 Custer Street',
                 'NCR Delhi',
             )
-        )
-
-
-def test_reg_form():
-    registration_page = RegistrationPage()
-    registration_page.open()
-
-    # browser.open('/automation-practice-form')
-    # browser.all('[id^=google_ads][id$=container__]').with_(
-    #     timeout=10).wait_until(
-    #     have.size_greater_than_or_equal(3)
-    # )
-    # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
-
-    # WHEN
-    registration_page.type_first_name('John')
-    # browser.element('#firstName').type('John')
-    browser.element('#lastName').type('Smith')
-
-    browser.element('#userEmail').type('name@example.com')
-
-    browser.all('[name=gender]').element_by(have.value('Male')).element(
-        '..').click()
-
-    browser.element('#userNumber').type('9079079079')
-
-    registration_page.fill_date_of_birth('1986', 'June', 15)
-    # browser.element('#dateOfBirthInput').click()
-    # browser.element('.react-datepicker__month-select').type('June')
-    # browser.element('.react-datepicker__year-select').type('1986')
-    # browser.element(f'.react-datepicker__day--0{15}').click()
-
-    browser.element('#subjectsInput').type('Computer Science').press_enter()
-
-    browser.element('[for=hobbies-checkbox-2]').perform(
-        command.js.scroll_into_view)
-    browser.element('[for=hobbies-checkbox-2]').click()
-
-
-    browser.element('#uploadPicture').set_value(
-        os.path.abspath(
-            os.path.join(os.path.dirname(tests.__file__), 'resources/foto.jpg')
-        )
-    )
-
-    browser.element('#currentAddress').type('1478 Custer Street')
-
-    browser.element('#state').click()
-    browser.all('[id^=react-select][id*=option]').element_by(
-        have.exact_text('NCR')
-    ).click()
-
-
-    browser.element('#city').click()
-    browser.all('[id^=react-select][id*=option]').element_by(
-        have.exact_text('Delhi')
-    ).click()
-
-
-    browser.element('#submit').perform(command.js.click)
-    '''
-
-    # THEN
-    '''
-    # can be considered as additional check:
-    browser.element('[id=example-modal-sizes-title-lg]').should(be.visible)
-    # OR (if you allow yourself to check app texts)
-    browser.element('[id=example-modal-sizes-title-lg]').should(
-        have.text('Thanks for submitting the form')
-    )
-
-    registration_page.should_registered_user_with(
-        'John Smith',
-        'name@example.com',
-        'Male',
-        '9079079079',
-        '15 June,1986',
-        'Computer Science',
-        'Reading',
-        'foto.jpg',
-        '1478 Custer Street',
-        'NCR Delhi',
         )
 
